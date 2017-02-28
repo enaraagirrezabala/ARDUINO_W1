1ae48d376e19d0d7f3ef29e25ef922a761e5c7fd
enaraagirrezabala <enaraagirrezabala@hotmail.com>
2017-02-28 12:29:55 +0100

    Update random_led.ino

--- random_led/random_led.ino
+++ random_led/random_led.ino
@@ -22,8 +22,7 @@ int led [] = {3, 5, 6, 9, 10};//Definimos el array que contiene los números de
 void setup() {
   // Definimos cada pin como output. Tendremos 4 pins por lo que el contador irá de 0
   //hasta llegar a 5, ya que la primera posición dentro del array es 0 (a0,a1,a2,a3)
-  // Abrir el puerto de seial.
-  Serial.begin(9600);
+
   for (int i = 0 ; i < 5; i++) {
     pinMode(led[i], OUTPUT);
   }
